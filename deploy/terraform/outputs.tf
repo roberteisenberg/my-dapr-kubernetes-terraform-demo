32c02bf38df96ff257bb36c8373d0d8ee72b57fd
roberteisenberg <robert@reassociates.net>
2024-06-02 07:18:42 -0700

    retreiving container registry primary key

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -40,3 +40,12 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
   sensitive = true
 }
+output "acr_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
+
+output "acr_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
