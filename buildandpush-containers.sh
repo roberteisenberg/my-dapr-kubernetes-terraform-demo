e2621ed9230f40a197a19ad7500d12c5a3e40771
roberteisenberg <robert@reassociates.net>
2025-04-12 14:11:41 -0700

    update

--- buildandpush-containers.sh
+++ buildandpush-containers.sh
@@ -1,31 +1,33 @@
+#!/bin/bash
+
 # 🔹 Build and Push Docker Images to Docker Hub:
 # - The following commands build and push Docker images for different services.
 # - Docker Hub is used as the container registry.
 # - Services include:
 #   - external-binding-sample
-#   - orderbackendimage
-#   - orderfrontendimage
-#   - pubsub-python-subscriber
+#   - dapr-traffic-control-order-backend
+#   - dapr-traffic-control-order-frontend
+#   - dapr-traffic-control-python-subscriber
 
 # 🔹 Commands:
 # - Docker build for each service.
-# - Docker tag with Docker Hub registry.
 # - Docker push to Docker Hub.
 
 # Ensure you're logged into Docker Hub
 docker login --username reisenberg123 --password Pr@gProg55
 
+cd external-binding
+docker build -t reisenberg123/external-binding-sample:1.0 .
+docker push reisenberg123/external-binding-sample:1.0
+
 cd ../order_backend
-docker build -t orderbackendimage .
-docker tag orderbackendimage reisenberg123/orderbackendimage
-docker push reisenberg123/orderbackendimage
+docker build -t reisenberg123/dapr-traffic-control-order-backend:1.0 .
+docker push reisenberg123/dapr-traffic-control-order-backend:1.0
 
 cd ../order_frontend
-docker build -t orderfrontendimage .
-docker tag orderfrontendimage reisenberg123/orderfrontendimage
-docker push reisenberg123/orderfrontendimage
+docker build -t reisenberg123/dapr-traffic-control-order-frontend:1.0 .
+docker push reisenberg123/dapr-traffic-control-order-frontend:1.0
 
 cd ../python-subscriber
-docker build -t pubsub-python-subscriber .
-docker tag pubsub-python-subscriber reisenberg123/pubsub-python-subscriber
-docker push reisenberg123/pubsub-python-subscriber
+docker build -t reisenberg123/dapr-traffic-control-python-subscriber:1.0 .
+docker push reisenberg123/dapr-traffic-control-python-subscriber:1.0
