c4505c32b37b063229005878683e50352f8c6136
roberteisenberg <robert@reassociates.net>
2025-04-13 01:07:44 -0700

    read 123

--- python-subscriber/app.py
+++ python-subscriber/app.py
@@ -37,4 +37,5 @@ def c_subscriber():
     print('Received message "{}" on topic "{}"'.format(request.json['data']['message'], request.json['topic']), flush=True)
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-app.run(port=5001)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
